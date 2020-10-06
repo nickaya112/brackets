7339d3a6a935dd2648fd80577c9c1db393197450
Nickita Solovei <nickaya112@gmail.com>
2020-10-06 18:31:56 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStr = str.split('');
+  let arrOpen = [];
+  let result = true;
+  arrStr.forEach(element => {
+    bracketsConfig.forEach(el => {
+      if (element === el[0] && element === el[1]) {
+        if (arrOpen[arrOpen.length - 1] === element) {
+          arrOpen.pop();
+        } else {
+          arrOpen.push(element);
+        }
+      } else if (element === el[0]) {
+        arrOpen.push(element)
+      } else if (element === el[1]) {
+        if (arrOpen[arrOpen.length - 1] === el[0] && arrOpen[0] !== undefined) {
+          arrOpen.pop();
+        } else {
+          result = false;
+        }
+      }
+    })
+  });
+  if (arrOpen.length === 0 && result) {
+    return true
+  } else return false;
+  
 }
